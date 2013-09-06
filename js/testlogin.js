b4fd6af4ee0b5a68e1fa980f5237aa1c108866fe
Tina Umlandt <tina.umlandt@jimdo.com>
2013-09-06 16:58:14 +0200

    refactoring

--- js/testlogin.js
+++ js/testlogin.js
@@ -1,6 +1,7 @@
 storage = chrome.storage.sync;
 
 function initView() {
+
 	var elements = document.getElementsByTagName("li");
 	for (i=0; i<elements.length; i++) {
 		var element = elements[i];
@@ -10,16 +11,26 @@ function initView() {
 	}
 }
 
-function beautifyElement(id, currentHTMLElement, isOdd) {
+function beautifyElement(id, currentHTMLLiElement, isOdd) {
+	currentHTMLLiElement.className = (isOdd ? ' odd ' : ' even ');
+
 	var surroundingSpan = document.createElement('span');
 	surroundingSpan.className = "testlogin";
-	currentHTMLElement.appendChild(surroundingSpan);
-	currentHTMLElement.className = (isOdd ? ' odd ' : ' even ')
+	currentHTMLLiElement.appendChild(surroundingSpan);
 	initCurrentDescription(id, surroundingSpan);
-	addEditTextField(id, surroundingSpan);
 }
 
-function showCurrentText(id, element, desc) {
+function initCurrentDescription(id, surroundingSpan) {
+
+	storage.get(null, function(result) {
+		var text = result[id];
+		text = text ? text : '';
+		addCurrentDescriptionField(id, surroundingSpan, text);
+		addEditTextField(id, surroundingSpan, text);
+	});
+}
+
+function addCurrentDescriptionField(id, element, desc) {
 	var textSpan = document.createElement('span');
 	textSpan.className = "currentDescription";
 	textSpan.id = "desc-" + id;
@@ -27,46 +38,36 @@ function showCurrentText(id, element, desc) {
 	element.appendChild(textSpan);	
 }
 
-function updateCurrentText(id, desc) {
-	var field = document.getElementById('desc-' + id);
-	if (field) {
-		field.innerHTML = desc;
-	}
-}
-
-function addEditTextField(id, element) {
+function addEditTextField(id, element, desc) {
 	var descriptionInput = document.createElement('input');
 	descriptionInput.name = "description";
 	descriptionInput.type = "text";
+	descriptionInput.value = desc;
 	descriptionInput.id = id;
-	descriptionInput.addEventListener("keyup", updateValue);
+	descriptionInput.addEventListener("keyup", updateDescriptionForId);
 	element.appendChild(descriptionInput);
 }
 
-function updateValue() {
+function updateDescriptionForId() {
 	var inputField = this;
 	var currentValue = inputField.value;
 	var currentId = inputField.id;
-	setCurrentDescription(currentId, currentValue);
+	saveUpdatedDescription(currentId, currentValue);
 }
 
-function initCurrentDescription(id, surroundingSpan) {
-
-	storage.get(null, function(result)
-	{
-		var text = result[id];
-		if(text){
-			showCurrentText(id, surroundingSpan, text);
-		}
-	});
-}
-
-function setCurrentDescription(id, desc) {
+function saveUpdatedDescription(id, desc) {
 	if (desc) {
 		var dataObj = {};
 		dataObj[id] = desc;
 		storage.set(dataObj);
 		updateCurrentText(id, desc);
+	}
+}
+
+function updateCurrentText(id, desc) {
+	var field = document.getElementById('desc-' + id);
+	if (field) {
+		field.innerHTML = desc;
 	}
 }
 
