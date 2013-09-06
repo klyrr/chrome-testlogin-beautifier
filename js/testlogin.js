051382d952707d736831b317c20d04568712c50b
Tina Umlandt <tina.umlandt@jimdo.com>
2013-09-06 16:33:58 +0200

    code clean up

--- js/testlogin.js
+++ js/testlogin.js
@@ -1,39 +1,33 @@
 storage = chrome.storage.sync;
 
-function addInputFields() {
-
-	var allSiteIds = {};
-	console.log("addInputFields");
-	initView();
-}
-
 function initView() {
-	// nothing yet
 	var elements = document.getElementsByTagName("li");
-	console.log("initView");
 	for (i=0; i<elements.length; i++) {
 		var element = elements[i];
 		var id = element.textContent;
-		beautifyElement(id, element);
+		var isOdd = i % 2 == 0;
+		beautifyElement(id, element, isOdd);
 	}
 }
 
-function beautifyElement(id, currentHTMLElement) {
+function beautifyElement(id, currentHTMLElement, isOdd) {
 	var surroundingSpan = document.createElement('span');
 	surroundingSpan.className = "testlogin";
 	currentHTMLElement.appendChild(surroundingSpan);
-	updateCurrentDescription(id, surroundingSpan);
-	addEditTextForm(id, surroundingSpan);
+	currentHTMLElement.className = (isOdd ? ' odd ' : ' even ')
+	initCurrentDescription(id, surroundingSpan);
+	addEditTextField(id, surroundingSpan);
 }
 
 function showCurrentText(id, element, desc) {
 	var textSpan = document.createElement('span');
 	textSpan.className = "currentDescription";
+	textSpan.id = "desc-" + id;
 	textSpan.innerHTML = desc;
 	element.appendChild(textSpan);	
 }
 
-function addEditTextForm(id, element) {
+function addEditTextField(id, element) {
 	var descriptionInput = document.createElement('input');
 	descriptionInput.name = "description";
 	descriptionInput.type = "text";
@@ -46,13 +40,12 @@ function updateValue() {
 	var inputField = this;
 	var currentValue = inputField.value;
 	var currentId = inputField.id;
-	console.log("fffd " + currentValue + " " + currentId);
 	setCurrentDescription(currentId, currentValue);
 }
 
-function updateCurrentDescription(id, surroundingSpan) {
+function initCurrentDescription(id, surroundingSpan) {
 
-	var foobar = storage.get(null, function(result)
+	storage.get(null, function(result)
 	{
 		var text = result[id];
 		if(text){
@@ -69,29 +62,4 @@ function setCurrentDescription(id, desc) {
 	}
 }
 
-function saveChanges() {
-
-	var nameOne = "al 2";
-	var nameTwo = "bob 3ssd3";
-	var nameThree = "carl";
-	var nameFour = "dan";
-
-	//var foo = {};
-	var dataObj = {};
-	//foo["id"] = dataObj;
-
-	dataObj[nameOne] = nameTwo;
-	//dataObj[nameThree] = nameFour;
-
-	storage.set(dataObj);
-
-	storage.get(null, function(result)
-	{
-		console.log(result[nameOne]);
-		console.log(result[nameThree]);
-	});
-
-}
-
-//saveChanges();
-addInputFields();
+initView();
